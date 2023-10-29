8c1c79bd26b12db7388755b3ad7ed7932788bb8c
Nisarat Aunreunngam <nisarata@Nisarats-MacBook-Pro.local>
2023-10-30 06:38:39 +0700

    SessionCookies

---
+++ src/main/java/int202/register/cookiessession/register/servlets/CourseListServlet.java
@@ -0,0 +1,19 @@
+package int202.register.cookiessession.register;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "CourseListServlet", value = "/CourseListServlet")
+public class CourseListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
+ 
